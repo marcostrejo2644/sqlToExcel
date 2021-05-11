5f1e54b953422c52eb4b5bfd637d0ed3a3e407a2
Lorenzo Lopez <Lorenzolopez200123@gmail.com>
2021-05-11 18:03:58 -0300

    añadir animacion dark to hover btn

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,2 +1,10 @@
 /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
-particlesJS.load('particles-js', '/js/particlesjs-config.json')
+particlesJS.load('particles-js', '/js/particlesjs-config.json');
+
+window.onload = function () {
+  const fondo = document.querySelector('#particles-js');
+  const boton = document.querySelector('#button-login');
+
+  boton.addEventListener('mouseover', () => fondo.classList.add('particles-dark'));
+  boton.addEventListener('mouseout', () => fondo.classList.remove('particles-dark'));
+};
